767abfbad8c352b85efaa5fdc48c5994131ace92
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2020-04-26 15:18:28 +0300

    fixed things they did not specify

--- DATAPATH/MEMSTAGE/MEMSTAGE.vhd
+++ DATAPATH/MEMSTAGE/MEMSTAGE.vhd
@@ -31,9 +31,24 @@ begin
 		if ByteOp = '0' then
 			MM_WrData <= MEM_DataIn;
 			MEM_DataOut <= MM_RdData;
-		elsif ByteOp = '1' then			
-			MM_WrData <= x"000000" & MEM_DataIn(7 downto 0);
-			MEM_DataOut <= x"000000" & MM_RdData(7 downto 0);
+		elsif ByteOp = '1' then	
+			case ALU_MEM_Addr(1 downto 0) is
+				when "00" =>
+					MM_WrData <= MM_RdData(31 downto 8) & MEM_DataIn(7 downto 0);
+					MEM_DataOut <= (23 downto 0 => MM_RdData(7)) & MM_RdData(7 downto 0);
+				when "01" =>
+					MM_WrData <= MM_RdData(31 downto 16) & MEM_DataIn(7 downto 0) & MM_RdData(7 downto 0);
+					MEM_DataOut <= (23 downto 0 => MM_RdData(7)) & MM_RdData(7 downto 0);
+				when "10" =>
+					MM_WrData <= MM_RdData(31 downto 24) & MEM_DataIn(7 downto 0) & MM_RdData(15 downto 0);
+					MEM_DataOut <= (23 downto 0 => MM_RdData(7)) & MM_RdData(7 downto 0);
+				when "11" =>
+					MM_WrData <= MEM_DataIn(7 downto 0) & MM_RdData(23 downto 0);
+					MEM_DataOut <= (23 downto 0 => MM_RdData(7)) & MM_RdData(7 downto 0);
+				when others =>
+					MM_WrData <= x"00000000";
+					MEM_DataOut <= x"00000000";
+			end case;
 		end if;
 	end process;	
 
